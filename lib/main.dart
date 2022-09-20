527bbd7f9bb5ec938c6127a65efd81ddf33c8e54
CheolheeLee0 <wks0968@gmail.com>
2022-09-20 21:49:11 +0900

    Set the reference design size ratio to iPhone 12 mini for easy UI development. (375, 812)

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,38 @@
 import 'package:cnubot_app/app_binding.dart';
 import 'package:cnubot_app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'cnubot',
-      initialRoute: Routes.HOME,
-      initialBinding: AppBinding(),
-      getPages: AppPages.routes,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'cnubot',
+          initialRoute: Routes.HOME,
+          initialBinding: AppBinding(),
+          getPages: AppPages.routes,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+        );
+      },
     );
   }
 }
