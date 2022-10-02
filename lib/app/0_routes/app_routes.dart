5e19948b37507a33c80c3778ee5e5783ed4bed41
CheolheeLee0 <wks0968@gmail.com>
2022-10-02 18:13:26 +0900

    refactor: change position of routes to posiition _paths

--- lib/app/0_routes/app_routes.dart
+++ lib/app/0_routes/app_routes.dart
@@ -1,11 +1,11 @@
 part of 'app_pages.dart';
 
-abstract class Routes {
-  static const splash = _Paths.splash;
-  static const home = _Paths.home;
-}
-
 abstract class _Paths {
   static const splash = '/';
   static const home = '/home';
+}
+
+abstract class Routes {
+  static const splash = _Paths.splash;
+  static const home = _Paths.home;
 }
