bc58bdba7ccfa66c891d0980c528780ad1e6c9d3
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-10-23 00:06:43 +0900

    fix: analyze 이슈 해결

--- lib/app/4_view/2_screen/5_search_detail/component/shortcut_menu_button.dart
+++ lib/app/4_view/2_screen/5_search_detail/component/shortcut_menu_button.dart
@@ -1,7 +1,6 @@
 import 'package:cnubot_app/app/4_view/0_constant/enum/shortcut_menu_type.dart';
 import 'package:cnubot_app/app/4_view/1_component/0_button/gray_round_button.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class ShortcutMenuButton extends StatelessWidget {
   const ShortcutMenuButton({Key? key, required this.shortcutMenuListType})
@@ -14,7 +13,6 @@ class ShortcutMenuButton extends StatelessWidget {
     return InkWell(
       onTap: () {
         //TODO: 학식까지 하고 라우팅을 바로 시켜줄 수 있어야함
-        print(shortcutMenuListType.displayName);
       },
       child: GrayRoundButton(
         text: shortcutMenuListType.displayName,
