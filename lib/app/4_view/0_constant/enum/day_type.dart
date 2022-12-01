110048401a850a4a656b8bd3a767adfbea666d50
CheolheeLee0 <wks0968@gmail.com>
2022-12-01 20:58:14 +0900

    feat: Add Day param

--- lib/app/4_view/0_constant/enum/day_type.dart
+++ lib/app/4_view/0_constant/enum/day_type.dart
@@ -1,16 +1,17 @@
 enum DayType {
-  today('오늘'),
-  mon('월'),
-  tue('화'),
-  wed('수'),
-  thu('목'),
-  fri('금'),
-  sat('토'),
-  sun('일'),
-  undefined('');
+  today('오늘', 'TODAY'),
+  mon('월', 'MONDAY'),
+  tue('화', 'TUESDAY'),
+  wed('수', 'WEDNESDAY'),
+  thu('목', 'THURSDAY'),
+  fri('금', 'FRIDAY'),
+  sat('토', 'SATURDAY'),
+  sun('일', 'SUNDAY'),
+  undefined('', '');
 
-  const DayType(this.displayName);
+  const DayType(this.displayName, this.param);
   final String displayName;
+  final String param;
 
   factory DayType.getByDisplayName(String displayName) {
     return DayType.values.firstWhere(
