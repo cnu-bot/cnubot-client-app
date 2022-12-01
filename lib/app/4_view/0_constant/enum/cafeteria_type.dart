7d5cc03c2aa9e6362a450672a2e086baba626290
CheolheeLee0 <wks0968@gmail.com>
2022-12-01 20:57:57 +0900

    feat: Add cafeteria param

--- lib/app/4_view/0_constant/enum/cafeteria_type.dart
+++ lib/app/4_view/0_constant/enum/cafeteria_type.dart
@@ -1,14 +1,15 @@
 enum CafeteriaType {
-  dormitory('기숙사'),
-  studentHall1('제1 학생회관'),
-  studentHall2('제2 학생회관'),
-  studentHall3('제3 학생회관'),
-  studentHall4('제4 학생회관'),
-  collegeOfLifeSciences('생활과학대학'),
-  undefined('');
+  dormitory('기숙사', 'DORMITORY'),
+  studentHall1('제1 학생회관', 'FIRST_STUDENT_HALL'),
+  studentHall2('제2 학생회관', 'SECOND_STUDENT_HALL'),
+  studentHall3('제3 학생회관', 'THIRD_STUDENT_HALL'),
+  studentHall4('제4 학생회관', 'FOURTH_STUDENT_HALL'),
+  collegeOfLifeSciences('생활과학대학', 'COLLEGE_OF_LIFE_SCIENCE'),
+  undefined('', '');
 
-  const CafeteriaType(this.displayName);
+  const CafeteriaType(this.displayName, this.param);
   final String displayName;
+  final String param;
 
   factory CafeteriaType.getByDisplayName(String displayName) {
     return CafeteriaType.values.firstWhere(
