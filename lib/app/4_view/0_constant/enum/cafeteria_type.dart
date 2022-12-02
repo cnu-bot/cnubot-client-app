f3299ddb64404db016524ae962f7c09c28a9e504
CheolheeLee0 <wks0968@gmail.com>
2022-12-03 02:42:46 +0900

    fix: change cafeteria_type enum name

--- lib/app/4_view/0_constant/enum/cafeteria_type.dart
+++ lib/app/4_view/0_constant/enum/cafeteria_type.dart
@@ -1,9 +1,9 @@
 enum CafeteriaType {
   dormitory('기숙사', 'DORMITORY'),
-  studentHall1('제1 학생회관', 'FIRST_STUDENT_HALL'),
-  studentHall2('제2 학생회관', 'SECOND_STUDENT_HALL'),
-  studentHall3('제3 학생회관', 'THIRD_STUDENT_HALL'),
-  studentHall4('제4 학생회관', 'FOURTH_STUDENT_HALL'),
+  firstSh('제1 학생회관', 'FIRST_STUDENT_HALL'),
+  secondSh('제2 학생회관', 'SECOND_STUDENT_HALL'),
+  thirdSh('제3 학생회관', 'THIRD_STUDENT_HALL'),
+  fourthSh('제4 학생회관', 'FOURTH_STUDENT_HALL'),
   collegeOfLifeSciences('생활과학대학', 'COLLEGE_OF_LIFE_SCIENCE'),
   undefined('', '');
 
@@ -14,6 +14,10 @@ enum CafeteriaType {
   factory CafeteriaType.getByDisplayName(String displayName) {
     return CafeteriaType.values.firstWhere(
         (value) => value.displayName == displayName,
+        orElse: () => CafeteriaType.undefined);
+  }
+  factory CafeteriaType.getByParam(String param) {
+    return CafeteriaType.values.firstWhere((value) => value.param == param,
         orElse: () => CafeteriaType.undefined);
   }
 }
